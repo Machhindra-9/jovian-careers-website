e7c6902af2521819cb230e94e30e80a5069b89c6
Machhindranath kangane <mac.kangane@gmail.com>
2025-11-03 21:45:34 +0530

    welcome

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    return "<h1>Welcome to Jovian website</h1>"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
