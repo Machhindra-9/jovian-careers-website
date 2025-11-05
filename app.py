fc794cc8d22d5016866ad3b2f0e20f3429abddfe
Machhindranath kangane <mac.kangane@gmail.com>
2025-11-05 08:48:29 +0530

    basic home page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
 def home():
-    return "<h1>Welcome to Jovian website</h1>"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
